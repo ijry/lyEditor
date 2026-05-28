aae4bbacf01650a98d012c2ff5a3c08e4660f04f
jry <ijry@qq.com>
2026-05-28 23:06:12 +0800

    docs: wire custom bilingual home landing into vitepress theme

--- docs-site/.vitepress/theme/index.ts
+++ docs-site/.vitepress/theme/index.ts
@@ -1,10 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import HomeEditorDemo from './components/HomeEditorDemo.vue'
+import HomeLanding from './components/HomeLanding.vue'
+import './home.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('HomeEditorDemo', HomeEditorDemo)
+    app.component('HomeLanding', HomeLanding)
   }
 } satisfies Theme
